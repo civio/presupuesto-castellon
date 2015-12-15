4038d6c6cd2b98b8402b100073e018b1e3cc119e
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-12-15 17:15:55 +0100

    Update badget loader to use institutional data

--- loaders/castellon_budget_loader.py
+++ loaders/castellon_budget_loader.py
@@ -48,6 +48,7 @@ class CastellonBudgetLoader(SimpleBudgetLoader):
         is_actual = (filename.find('/ejecucion_')!=-1)
         if is_expense:
             fc_code = self.clean(line[1]).zfill(4)          # Fill with zeroes on the left if needed
+            ic_code = self.clean(line[0]).zfill(3)          # Fill with zeroes on the left if needed
 
             # Remove last char to fc_code if grather than 4 chars
             if len(fc_code) == 5:
@@ -55,17 +56,21 @@ class CastellonBudgetLoader(SimpleBudgetLoader):
 
             # For years before 2015 we check whether we need to amend the programme code
             year = re.search('municipio/(\d+)/', filename).group(1)
-            if year in ['2011', '2012', '2013', '2014']:
+            if year in ['2013', '2014']:
                 new_programme = programme_mapping.get(fc_code)
                 if new_programme:
                     fc_code = new_programme
+
+            # For years before 2016 we check whether we need to amend the institutional code
+            if year not in ['2013', '2014', '2015']:
+                ic_code = '1'+ic_code[1:]
 
             return {
                 'is_expense': True,
                 'is_actual': is_actual,
                 'fc_code': fc_code,
                 'ec_code': self.clean(line[2]),
-                'ic_code': '100',
+                'ic_code': ic_code,
                 'item_number': self.clean(line[2])[-2:],    # Last two digits
                 'description': line[3],
                 'amount': self._parse_amount(line[7 if is_actual else 4])
@@ -76,17 +81,8 @@ class CastellonBudgetLoader(SimpleBudgetLoader):
                 'is_expense': False,
                 'is_actual': is_actual,
                 'ec_code': self.clean(line[1]),
-                'ic_code': '100',                           # All income goes to the root node
+                'ic_code': '999',                           # All income goes to the root node (009)
                 'item_number': self.clean(line[1])[-2:],    # Last two digits
                 'description': line[2],
                 'amount': self._parse_amount(line[6 if is_actual else 3])
             }
-
-    # We don't have an institutional breakdown in Torrelodones, so we create just a catch-all organism.
-    # (We then configure the theme so we don't show an institutional breakdown anywhere.)
-    def load_institutional_classification(self, path, budget):
-        InstitutionalCategory(  institution='1',
-                                section='10',
-                                department='100',
-                                description='Ayuntamiento de Castelló de la Plana',
-                                budget=budget).save()
