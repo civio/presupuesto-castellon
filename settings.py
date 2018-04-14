34d9aa82b68fa7f4f27bbd740affbd2243281ea8
Eduardo Sebastian <esebastian@altiora.es>
2018-04-14 08:10:58 +0200

    Add guided visit section textual content
    civio/presupuesto-management#680

--- settings.py
+++ settings.py
@@ -1,6 +1,5 @@
 # -*- coding: UTF-8 -*-
-from django.conf.urls import patterns, url
-#from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls import url
 
 
 MAIN_ENTITY_LEVEL = 'municipio'
@@ -21,11 +20,11 @@ PAYMENTS_LOADER = 'CastellonPaymentsLoader'
 # Show Payments section in menu & home options. Default: False.
 # SHOW_PAYMENTS = True
 
-# Configure 'by area' payment breakdown. Default: ['area', 'payee', 'description']
-# PAYMENTS_BREAKDOWN_BY_AREA = ['area', 'payee', 'description']
+# Configure 'by area' payment breakdown. Default: ['area', 'payee', 'description']
+# PAYMENTS_BREAKDOWN_BY_AREA = ['area', 'payee', 'description']
 
-# Configure 'by payee' payment breakdown. Default: ['payee', 'area', 'description']
-# PAYMENTS_BREAKDOWN_BY_PAYEE = ['payee', 'area', 'description']
+# Configure 'by payee' payment breakdown. Default: ['payee', 'area', 'description']
+# PAYMENTS_BREAKDOWN_BY_PAYEE = ['payee', 'area', 'description']
 
 # Show Tax Receipt section in menu & home options. Default: False.
 SHOW_TAX_RECEIPT = True
@@ -85,40 +84,40 @@ LANGUAGES = (
 )
 
 # Facebook Aplication ID used in social_sharing temaplate. Default: ''
-# In order to get the ID create an app in https://developers.facebook.com/
-FACEBOOK_ID             = '297629970407368'
+# In order to get the ID create an app in https://developers.facebook.com/
+FACEBOOK_ID = '297629970407368'
 
 # Google Analytics ID. Default: ''
-# In order to get the ID create a Google Analytics Acount in https://analytics.google.com/analytics/web/
-ANALYTICS_ID            = 'UA-73230322-1'
+# In order to get the ID create a Google Analytics Acount in https://analytics.google.com/analytics/web/
+ANALYTICS_ID = 'UA-73230322-1'
 
 # Setup Data Source Budget link
-DATA_SOURCE_BUDGET      = 'http://castello.es/web30/pages/seccion_web10.php?cod1=8'
+DATA_SOURCE_BUDGET = 'http://castello.es/web30/pages/seccion_web10.php?cod1=8'
 
 # Setup Data Source Population link
-DATA_SOURCE_POPULATION  = 'http://www.ine.es/jaxiT3/Tabla.htm?t=2865'
+DATA_SOURCE_POPULATION = 'http://www.ine.es/jaxiT3/Tabla.htm?t=2865'
 
 # Setup Data Source Inflation link
-DATA_SOURCE_INFLATION   = 'http://www.ine.es/jaxiT3/Tabla.htm?t=2605'
+DATA_SOURCE_INFLATION = 'http://www.ine.es/jaxiT3/Tabla.htm?t=2605'
 
 # Setup Main Entity Web Url
-MAIN_ENTITY_WEB_URL     = 'http://www.castello.es/'
+MAIN_ENTITY_WEB_URL = 'http://www.castello.es/'
 
 # Setup Main Entity Legal Url (if empty we hide the link)
-MAIN_ENTITY_LEGAL_URL   = ''
+MAIN_ENTITY_LEGAL_URL = ''
 
 # Setup Main Entity Legal Url (if empty we hide the link)
 MAIN_ENTITY_PRIVACY_URL = 'http://www.castello.es/web30/pages/generico_web10.php?cod1=437&cod2=583'
 
 # External URL for Cookies Policy (if empty we use out template page/cookies.html)
-COOKIES_URL             = ''
+COOKIES_URL = ''
 
 # We can define additional URLs applicable only to the theme. These will get added
 # to the project URL patterns list.
 # Must be needed to uncomment 3rd line in order to import i18n_patterns
-# EXTRA_URLS = i18n_patterns('presupuesto-base.views',
-#     url(r'^visita-guiada$', 'guidedvisit', name="guidedvisit"),
-# )
+EXTRA_URLS = (
+    url(r'^visita-guiada$', 'guidedvisit', name="guidedvisit"),
+)
 
 
 # Welcome Settings
@@ -154,7 +153,7 @@ OVERVIEW_EXPENSE_NODES = ['23', '92', '13', '16', '15', '01', '33', '32', '44', 
 # OVERVIEW_RELAX_FACTOR = 0.5
 
 # Adjust inflation in amounts in Overview page. Default: True
-# ADJUST_INFLATION_IN_OVERVIEW = False
+# ADJUST_INFLATION_IN_OVERVIEW = False
 
 # Show Subtotals panel in Overview. Default: False
 # SHOW_OVERVIEW_SUBTOTALS = True
@@ -167,13 +166,13 @@ CALCULATE_BUDGET_INDICATORS = False
 # ----------------------
 
 # Treemaps minimum height or width to show labels. Default: 70 (Optional)
-# TREEMAP_LABELS_MIN_SIZE = 70
+# TREEMAP_LABELS_MIN_SIZE = 70
 
 # Treemap minimum font size. Default: 11 (Optional)
 # TREEMAP_LABELS_FONT_SIZE_MIN = 11
 
 # Allow overriding of default treemap color scheme
-# COLOR_SCALE = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#e7969c', '#bcbd22', '#17becf']
+# COLOR_SCALE = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#e7969c', '#bcbd22', '#17becf']
 
 # How many levels to show in the global institutional treemap? Default: 1.
-# INSTITUTIONAL_MAX_LEVELS = 2
+# INSTITUTIONAL_MAX_LEVELS = 2
